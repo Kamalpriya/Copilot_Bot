378ed4be3a5e795923e6b7de210b273325dbfd0a
Kamalpriya CM <kamalpriya.cm@gmail.com>
2023-08-17 04:02:25 +0530

    Add project files.

---
+++ SG_Bot/Bots/DialogBot.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.18.1
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+using SG_Bot.Models;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace SG_Bot.Bots
+{
+    // This IBot implementation can run any type of Dialog. The use of type parameterization is to allows multiple different bots
+    // to be run at different endpoints within the same project. This can be achieved by defining distinct Controller types
+    // each with dependency on distinct IBot types, this way ASP Dependency Injection can glue everything together without ambiguity.
+    // The ConversationState is used by the Dialog system. The UserState isn't, however, it might have been used in a Dialog implementation,
+    // and the requirement is that all BotState objects are saved at the end of a turn.
+    public class DialogBot<T> : ActivityHandler
+        where T : Dialog
+    {
+#pragma warning disable SA1401 // Fields should be private
+        protected readonly Dialog Dialog;
+        protected readonly BotState ConversationState;
+        protected readonly BotState UserState;
+        protected readonly ILogger Logger;
+#pragma warning restore SA1401 // Fields should be private
+
+        public DialogBot(ConversationState conversationState, UserState userState, T dialog, ILogger<DialogBot<T>> logger)
+        {
+            ConversationState = conversationState;
+            UserState = userState;
+            Dialog = dialog;
+            Logger = logger;
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            // Save any state changes that might have occurred during the turn.
+            await ConversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await UserState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            Logger.LogInformation("Running dialog with Message Activity.");
+
+            // Run the Dialog with the new message Activity.
+            // await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+            
+            string replyText = await RunGPT(turnContext.Activity.Text);
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        private async Task<string> RunGPT(string query)
+        {
+            string OutPutResult = "";
+            CompletionRequest completionRequest = new CompletionRequest
+            {
+                Model = "text-davinci-003",
+                Prompt = query,
+                MaxTokens = 120
+            };
+            CompletionResponse completionResponse = new CompletionResponse();
+            using (HttpClient httpClient = new HttpClient())
+            {
+                using (var httpReq = new HttpRequestMessage(HttpMethod.Post, "https://api.openai.com/v1/completions"))
+                {
+                    httpReq.Headers.Add("Authorization", "Bearer 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");
+                    string requestString = JsonSerializer.Serialize(completionRequest);
+                    httpReq.Content = new StringContent(requestString, Encoding.UTF8, "application/json");
+                    using (HttpResponseMessage? httpResponse = await httpClient.SendAsync(httpReq))
+                    {
+                        if (httpResponse is not null)
+                        {
+                            if (httpResponse.IsSuccessStatusCode)
+                            {
+                                string responseString = await httpResponse.Content.ReadAsStringAsync();
+                                {
+                                    if (!string.IsNullOrWhiteSpace(responseString))
+                                    {
+                                        completionResponse = JsonSerializer.Deserialize<CompletionResponse>(responseString);
+                                    }
+                                }
+                            }
+                        }
+                        if (completionResponse is not null)
+                        {
+                            string? completionText = completionResponse.Choices?[0]?.Text;
+                            return completionText;
+                        }
+                    }
+                }
+            }
+
+            return OutPutResult;
+        }
+    }
+}
